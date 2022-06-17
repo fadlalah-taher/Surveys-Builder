53abc9e523136f75d6a11757e8dc47bfdca027f4
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-17 21:17:13 +0300

    Adding api function getSurveyById

--- laravel-server/app/Http/Controllers/SurveyController.php
+++ laravel-server/app/Http/Controllers/SurveyController.php
@@ -9,11 +9,21 @@ class SurveyController extends Controller
 {
     public function getSurvey(){
         $survey = Survey::all();
+
         return response()->json([
             "success" => true,
             "items" => $survey
         ], 200);
     }
 
-    
+    public function getSurveyById(Request $request){
+        $id = $request->survey_id;
+        $survey = Survey::find($id);
+
+        return response()->json([
+            "success" => true,
+            "items" => $survey
+        ], 200);
+    }
+
 }
