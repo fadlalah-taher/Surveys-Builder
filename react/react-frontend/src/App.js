db6797f6c7a26aa15d4fbbb0b65f3746ffd5a331
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-17 11:30:53 +0300

    Adding Register.js and fetch

--- react/react-frontend/src/App.js
+++ react/react-frontend/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login';
+import Register from './components/Register'
+import { useState } from "react";
+//import Register from './components/Register';
 
 function App() {
+
+   // Initialize State
+   const [tasks, setTasks] = useState([]);
+
+  //Adding a Task
+  const addTask = async (e) => {
+    const res = await fetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(e),
+    });
+    const data = await res.json();
+    console.log(data);
+    setTasks([...tasks, data]);
+  };
+
   return (
     <BrowserRouter>
     <div>
+      <Register onAdd={addTask} />
       <Login/>
     </div>
     </BrowserRouter>
---
+++ react/react-frontend/src/components/Register.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useState } from "react";
+
+const Register = ({onAdd}) => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+   //Add Data to Backend on Submit
+   const onSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      alert("Please add a task!");
+      return;
+    }
+    onAdd({ name,email, password});
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Full Name</label>
+        <input
+          type="text"
+          placeholder={"Add Task"}
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="form-control">
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder={"Email"}
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="form-control">
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder={"Password"}
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      
+      <input type={"submit"} value="Register" className="btn btn-block" />
+    </form>
+  )
+}
+
+export default Register
