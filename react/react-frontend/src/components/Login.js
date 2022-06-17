604754952fd07e9a2bbfc11a690737cba11b957d
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-17 09:38:09 +0300

    Adding login,Register.js (components)

---
+++ react/react-frontend/src/components/Login.js
@@ -0,0 +1,15 @@
+import { useState, useEffect} from 'react';
+
+
+const Login = () => {
+    const [user , setUser] = useState("");
+    const [pwd , setPwd] = useState("");
+    //const [user , setUser] = useState("");
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Login
