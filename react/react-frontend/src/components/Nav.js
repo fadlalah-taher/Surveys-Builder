7baa53a32876fd38371b3006b0dfcd10d517313e
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-18 21:50:29 +0300

    Adding axios function (logout)

--- react/react-frontend/src/components/Nav.js
+++ react/react-frontend/src/components/Nav.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Logo from '../images/logo.jpg';
+import axios from 'axios';
+
 //import logo from './logo.png'
 const Nav = () => {
+  const logout = async (e) => {
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/v1/logout',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+        'Accept':'application/json'
+      },
+    })
+    .then(function(response){
+      localStorage.setItem('access_token',null);
+      window.location = "/";
+    }).catch(function(response){
+      alert("You are not logged in!")
+    })
+  }
+  
   return (
     <div className='nav'>
       <div className='left'>
             <img className='logo' src={Logo} alt="Logo"/>
       </div>
-      <input type={"submit"} value="Logout" className="btn btn-logout" />
+      <input type={"submit"} onClick={logout} value="Logout" className="btn btn-logout" />
     </div>
   )
 }
