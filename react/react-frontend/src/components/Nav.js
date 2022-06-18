438fb57046073177581046eeff77a193b3ed2299
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-18 21:03:25 +0300

    Adding logo and Updating index.css

--- react/react-frontend/src/components/Nav.js
+++ react/react-frontend/src/components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from '../images/logo.jpg';
 //import logo from './logo.png'
 const Nav = () => {
   return (
     <div className='nav'>
       <div className='left'>
-            {/* <img className='logo' src={logo} alt="Logo"/> */}
+            <img className='logo' src={Logo} alt="Logo"/>
       </div>
       <div></div>
     </div>
