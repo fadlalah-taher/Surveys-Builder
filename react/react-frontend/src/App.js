e01ce74d3a56a46dd76a1e3365634d65cb90df2c
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-18 16:50:49 +0300

    Fetching all surveys

--- react/react-frontend/src/App.js
+++ react/react-frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from './components/Login';
 import Register from './components/Register'
 import Nav from './components/Nav';
 import axios from 'axios';
 import DisplaySurvey from './components/DisplaySurvey';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddSurvey from './components/AddSurvey';
 
 //import Register from './components/Register';
@@ -13,7 +14,30 @@ import AddSurvey from './components/AddSurvey';
 function App() {
 
    // Initialize State
-   const [users, setUsers] = useState([]);
+   //const [users, setUsers] = useState([]);
+   // Initialize State
+  const [surveys, setSurveys] = useState([]);
+
+  // Initialize all tasks into state from backend at component load
+  useEffect(() => {
+    const getTasks = async () => {
+      const surveyssFromServer = await fetchTasks();
+      console.log(surveyssFromServer);
+      setSurveys(surveyssFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //Fetch All Tasks from Backend
+  const fetchTasks = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/v1/getsurveys");
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   //Adding a Task
   const addTask = async (e) => {
--- react/react-frontend/src/components/DisplaySurvey.js
+++ react/react-frontend/src/components/DisplaySurvey.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import axios from 'axios';
 
-const DisplaySurvey = () => {
+const DisplaySurvey = ({surveys}) => {
 
 
   return (
     <div className='display-survey'>
-      <div className='survey-card'>
+      {surveys.map((survey) => (
+        <div className='survey-card'>
+        <div className='title-survey'>
+            <p>Hello word</p>
+        </div>
+        <div>
+            <input type={"submit"} value="Start" className="btn btn--survey" />
+        </div>
+      </div>
+        // <Task
+        //   key={survey.id}
+        //   task={survey}
+        // //   onDelete={onDelete}
+        // //   onToggle={onToggle}
+        // />
+      ))}
+    </div>
+  )
+}
+
+export default DisplaySurvey
+
+{/* <div className='survey-card'>
         <div className='title-survey'>
             <p>Hello word</p>
         </div>
@@ -37,9 +59,4 @@ const DisplaySurvey = () => {
         <div>
             <input type={"submit"} value="Start" className="btn btn--survey" />
         </div>
-      </div>
-    </div>
-  )
-}
-
-export default DisplaySurvey
+      </div> */}
