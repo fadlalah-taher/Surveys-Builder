cd26adfffc59308827a2dfc971d40c6ff56d87cb
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-19 21:38:23 +0300

    Updating the direction of page

--- react/react-frontend/src/components/DisplaySurvey.js
+++ react/react-frontend/src/components/DisplaySurvey.js
@@ -4,29 +4,24 @@ import { Link, useLinkClickHandler } from 'react-router-dom';
 //import axios from 'axios';
 
 const DisplaySurvey = ({surveys}) => {
-  //const [id , setId] = useState("");
 
   function redirectSurvey (sId) {
     localStorage.setItem("survey_id", sId);
-    window.location="/Survey";
+    window.location="/QuestionContainer";
   };
   return (
     <div className='display-survey'>
-    {/* <> */}
       {surveys.map((survey, index) => (
         
         <div className='survey-card'>
             <div className='title-survey'>
                 <p>{survey.title}</p>
-                {/* <p>{survey.id}</p> */}
             </div>
             <div>
-              {/* {localStorage.setItem("survey_id",survey.id)} */}
                 <input type={"submit"} key={index} value="Start" onClick={() => redirectSurvey(survey.id)}  className="btn btn--survey" />
             </div>
         </div>
       ))}
-      {/* </> */}
     </div>
   )
 }
