223a1d55dd2fd756d8908ecb9b0f44495471c872
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-20 12:09:08 +0300

    Updating QuestionItem.js

--- react/react-frontend/src/components/QuestionItem.js
+++ react/react-frontend/src/components/QuestionItem.js
@@ -14,7 +14,7 @@ const QuestionItem = ({text, question_id, type}) => {
                 setOptions(mydata['options']);
             })
         } catch(err){
-            console.log(err);
+            //console.log(err);
         }
     }
 
@@ -28,15 +28,13 @@ const QuestionItem = ({text, question_id, type}) => {
             var mcqs = document.getElementsByName('q' + question_id);
             for (var x of mcqs) {
                 if (x.checked) {
-                     console.log(x.checked)
-                     console.log(x.value)
                     try {
                         await axios.post(`http://127.0.0.1:8000/api/v1/addanswer`, {
                             question_id: question_id,
                             text: x.value
                         })
                         .then(res => {
-                            console.log("mcq added");
+                            window.location="/DisplaySurvey";
                         })
                     } catch (err) {
                          console.log(err)
@@ -69,14 +67,13 @@ const QuestionItem = ({text, question_id, type}) => {
         document.getElementById('answer_btn').addEventListener("click", async function () {
             // console.log(question_id)
             var text_ansr = document.getElementById('q' + question_id).value;
-             console.log(text_ansr)
             try {
                 await axios.post(`http://127.0.0.1:8000/api/v1/addanswer`, {
                     question_id: question_id,
                     text: text_ansr
                 })
                 .then(res => {
-                    console.log("text added");
+                    window.location="/DisplaySurvey";
                 })
             } catch (err) {
                  console.log(err)
@@ -101,15 +98,13 @@ const QuestionItem = ({text, question_id, type}) => {
             var chbs = document.getElementsByName('q' + question_id);
             for (var x of chbs) {
                 if (x.checked) {
-                     console.log(x.value);
-                     console.log(x.name);
                     try {
                         await axios.post(`http://127.0.0.1:8000/api/v1/addanswer`, {
                             question_id: question_id,
                             text: x.value
                         })
                         .then(res => {
-                            console.log("checkbox added");
+                            window.location="/DisplaySurvey";
                         })
                     } catch (err) {
                          console.log(err)
